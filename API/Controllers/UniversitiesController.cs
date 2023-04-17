4c3057650df1ded70386f4ae8556083d51fa022f
Fahri Hanif <fahrihanif03@gmail.com>
2023-04-17 18:54:53 +0700

    Create UniversitiesController.cs

---
+++ API/Controllers/UniversitiesController.cs
@@ -0,0 +1,40 @@
+﻿using API.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace API.Controllers;
+//https://domain/api/universities
+[Route("api/[controller]")]
+[ApiController]
+public class UniversitiesController : ControllerBase
+{
+    private readonly IUniversityRepository _universityRepository;
+
+    public UniversitiesController(IUniversityRepository universityRepository)
+    {
+        _universityRepository = universityRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var results = await _universityRepository.GetAllAsync();
+        if (results == null) {
+            return NotFound(new {
+                code = StatusCodes.Status404NotFound,
+                status = HttpStatusCode.NotFound.ToString(),
+                data = new {
+                    message = "Data Not Found!"
+                }
+            });
+        }
+
+        return Ok(new {
+            code = StatusCodes.Status200OK,
+            status = HttpStatusCode.OK.ToString(),
+            data = new {
+                message = "Insert Success"
+            }
+        });
+    }
+}
